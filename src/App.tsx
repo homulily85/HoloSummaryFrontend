8bb536d2ebc2ef3f18456bd15824acb1807dd78d
homulily85 <157974079+homulily85@users.noreply.github.com>
2026-05-24 01:55:03 +0700

    feat: initialize frontend project with React, Vite, and Tailwind CSS
    - Added package.json with dependencies and scripts for development, build, linting, and preview.
    - Created favicon.svg for the application.
    - Added icons.svg containing various SVG icons for use in the application.
    - Implemented main application structure in App.tsx.
    - Developed Topbar component with mobile search functionality.
    - Included Tailwind CSS in index.css for styling.
    - Set up main entry point in main.tsx to render the application.
    - Configured TypeScript with tsconfig files for app and node environments.
    - Established Vite configuration for React and Tailwind CSS integration.

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Topbar from "./components/topbar";
+
+function App() {
+    return (
+        <div className='grid grid-cols-[1fr_6fr] h-screen'>
+            <div className='col-span-2'>
+                <Topbar />
+            </div>
+        </div>
+    );
+}
+export default App;
