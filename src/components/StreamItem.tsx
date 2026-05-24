e6f643002d3875e1adc84d9ff00ddee54f131392
homulily85 <157974079+homulily85@users.noreply.github.com>
2026-05-25 01:39:12 +0700

    feat: wrap StreamItem content in anchor tag for improved navigation

--- src/components/StreamItem.tsx
+++ src/components/StreamItem.tsx
@@ -17,32 +17,38 @@ function StreamItem({ video }: { video: VideoStream }) {
     }, []);
 
     return (
-        <div className='flex flex-col gap-2 p-2 hover:bg-gray-100 rounded-md cursor-pointer'>
-            <div className='relative aspect-video rounded-md overflow-hidden mb-2'>
-                <img
-                    src={`https://i.ytimg.com/vi/${video.id}/mqdefault.jpg`}
-                    alt={`${video.title} Thumbnail`}
-                    className='w-full h-full object-cover'
-                />
-                <div
-                    className={`${video.status !== "past" ? "hidden" : "absolute bottom-1 right-1 bg-gray-700 bg-opacity-70 text-white text-xs px-2 py-0.5 rounded"}`}>
-                    {toHumanReadableDuration(video.duration)}
+        <a
+            href={`https://www.youtube.com/watch?v=${video.id}`}
+            target='_blank'
+            rel='noopener noreferrer'>
+            <div className='flex flex-col gap-2 p-2 hover:bg-gray-100 rounded-md cursor-pointer'>
+                <div className='relative aspect-video rounded-md overflow-hidden mb-2'>
+                    <img
+                        src={`https://i.ytimg.com/vi/${video.id}/mqdefault.jpg`}
+                        alt={`${video.title} Thumbnail`}
+                        className='w-full h-full object-cover'
+                    />
+                    <div
+                        className={`${video.status !== "past" ? "hidden" : "absolute bottom-1 right-1 bg-gray-700 bg-opacity-70 text-white text-xs px-2 py-0.5 rounded"}`}>
+                        {toHumanReadableDuration(video.duration)}
+                    </div>
                 </div>
+                <h3 className='text-lg font-semibold line-clamp-2 overflow-hidden'>
+                    {video.title}
+                </h3>
+                <p className='text-sm font-semibold text-gray-600'>
+                    {`${video.channel.englishName || video.channel.name}`}
+                </p>
+                <p className='text-sm text-gray-600'>
+                    {video.status === "live"
+                        ? "Live - Started at " +
+                          video.availableAt.toLocaleString()
+                        : video.status === "upcoming"
+                          ? `Starts at ${video.availableAt.toLocaleString()}`
+                          : `Started at ${video.availableAt.toLocaleString()}`}
+                </p>
             </div>
-            <h3 className='text-lg font-semibold line-clamp-2 overflow-hidden'>
-                {video.title}
-            </h3>
-            <p className='text-sm font-semibold text-gray-600'>
-                {`${video.channel.englishName || video.channel.name}`}
-            </p>
-            <p className='text-sm text-gray-600'>
-                {video.status === "live"
-                    ? "Live - Started at " + video.availableAt.toLocaleString()
-                    : video.status === "upcoming"
-                      ? `Starts at ${video.availableAt.toLocaleString()}`
-                      : `Started at ${video.availableAt.toLocaleString()}`}
-            </p>
-        </div>
+        </a>
     );
 }
 export default StreamItem;
