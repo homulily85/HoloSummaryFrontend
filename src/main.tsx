1fe5eef51ea926a81de2f38a27dd6a03345c8bab
homulily85 <157974079+homulily85@users.noreply.github.com>
2026-07-05 02:29:17 +0700

    fix: replace BrowserRouter with HashRouter for routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Routes, Route, HashRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./index.css";
 
 import App from "./App.tsx";
@@ -18,11 +18,11 @@ const queryClient = new QueryClient({
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
         <QueryClientProvider client={queryClient}>
-            <BrowserRouter>
+            <HashRouter>
                 <Routes>
                     <Route path='/*' element={<App />} />
                 </Routes>
-            </BrowserRouter>
+            </HashRouter>
         </QueryClientProvider>
     </StrictMode>,
 );
